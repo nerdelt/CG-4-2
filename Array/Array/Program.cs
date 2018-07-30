074d311e0b8ad3710e852d7034352b458677b0d0
nerdelt <nerdelt@gmail.com>
2018-07-30 17:37:27 -0500

    Assignment 4-2

---
+++ Array/Array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the array with given numbers 
+            int[] numbers = new int[6] { 1, 1, 2, 3, 5, 8 };
+
+            //create a loop to print each array
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
